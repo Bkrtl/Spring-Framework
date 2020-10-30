3b35f80d6d15a17f57f5a2ee86bba0db2299d324
Bekir Kartal <bkrprs@icloud.com>
2020-10-30 03:41:05 +0100

    Java Code-Configuration

---
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,20 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
+
+
+public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: " +(30+extraSessions.getHours()));
+    }
+}
